023ca27c1840dec2b5ffd17ebe8ff782975c23a3
PhilFuster <pafus23@protonmail.com>
2020-04-03 00:58:02 -0400

    Edited styles for buttons and changed the way guess_form was structured.

--- public/guess_history.js
+++ public/guess_history.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
         }
         console.log("here");
     })
-    $('#history').on("click", showHistory);
+    $('#history').on("click", showHistory).hide();
     
 
     function showHistory() {
